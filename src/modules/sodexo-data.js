7dc1c12477379fd739201193c786a14f86395a14
Nakki <nakkikone@Nakkikone.local>
2020-03-08 10:25:15 +0200

    vanha data ruokalistaan

--- src/modules/sodexo-data.js
+++ src/modules/sodexo-data.js
@@ -7,8 +7,8 @@ const coursesEn = [];
 const today = yyyy + '-' + mm + '-' +dd;
 
 const getMenus = async () => {
-    // const response = await getJsonMenu('https://www.sodexo.fi/ruokalistat/output/daily_json/152/2020-02-21');// Backup link for weekends
-    const response = await getJsonMenu('https://www.sodexo.fi/ruokalistat/output/daily_json/152/' + today);
+    const response = await getJsonMenu('https://www.sodexo.fi/ruokalistat/output/daily_json/152/2020-03-06');// Backup link for weekends
+    //const response = await getJsonMenu('https://www.sodexo.fi/ruokalistat/output/daily_json/152/' + today);
     const menu = await response;
     parseMenus(menu);
 };
