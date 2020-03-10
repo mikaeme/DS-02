fb3dd4d9350bca6a99cc2952a5003d262ead522a
Ida Laasasenaho <ida.laasasenaho@gmail.com>
2020-03-10 15:01:55 +0200

    carousel

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 'use strict';
 
-/*import { nextSlide } from './modules/carousel/carousel';*/
+import { nextSlide } from './modules/carousel/carousel';
 import { getLunchMenu } from './modules/lunch/main-module';
 import { getHsl } from './modules/hsl/main-module';
 import { getWeather } from './modules/weather/weather';
 import { getNews } from './modules/news/teksti-tv';
-import { getAltNews } from './modules/news/news';
+
 
 const body = document.querySelector('body');
 
-/*nextSlide();*/
+
+// let carouselCounter = 0;
+// let carousel = setInterval(() =>  {
+// carouselCounter++;
+// console.log(carouselCounter);
+// if (carouselCounter === 1) {
+//   document.querySelector('.slides').innerHTML = ('./assets/img/ds1.gif');
+// }
+// if (carouselCounter === 5) {
+//   document.querySelector('.slides').innerHTML = ('./assets/img/ds1.gif');
+// }
+// if (carouselCounter === 1) {
+//   document.querySelector('.slides').innerHTML = ('./assets/img/ds1.gif');
+// }
+// });
+
+nextSlide();
 getHsl();
 getWeather();
 getNews();
 getLunchMenu();
-getAltNews();
 
 export { body };
