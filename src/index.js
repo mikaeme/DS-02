36a5fc4a5e588e12481b62687739183e7240d05a
Nakki <nakkikone@Nakkikone.local>
2020-03-10 23:22:03 +0200

    hsl-lisäyksiä

--- src/index.js
+++ src/index.js
@@ -6,15 +6,9 @@ import { getLunchMenu } from './modules/lunch/main-module';
 import { getHsl } from './modules/hsl/main-module';
 import { getWeather } from './modules/weather/weather';
 import { getNews } from './modules/news/teksti-tv';
-<<<<<<< HEAD
-import { getSNews } from './modules/news/sports';
-import { getFNews } from './modules/news/foreign';
-
-=======
 import {getSNews} from './modules/news/sports';
 import {getFNews} from './modules/news/foreign';
 import { openAdminPanel } from './modules/admin/admin';
->>>>>>> 1c7b7d6125007d06171030a7939cb5e484699c80
 
 const body = document.querySelector('body');
 
@@ -54,8 +48,4 @@ getWeather();
 // getNews();
 getLunchMenu();
 
-<<<<<<< HEAD
-export { body };
-=======
 export { body, getLunchMenu, getHsl };
->>>>>>> 1c7b7d6125007d06171030a7939cb5e484699c80
