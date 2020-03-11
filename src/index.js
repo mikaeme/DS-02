a31055da678c8964c1b4fc235d924fb74d7c4f17
Ida Laasasenaho <ida.laasasenaho@gmail.com>
2020-03-11 08:41:26 +0200

    ttext line height

--- src/index.js
+++ src/index.js
@@ -6,15 +6,11 @@ import { getLunchMenu } from './modules/lunch/main-module';
 import { getHsl } from './modules/hsl/main-module';
 import { getWeather } from './modules/weather/weather';
 import { getNews } from './modules/news/teksti-tv';
-<<<<<<< HEAD
 import { getSNews } from './modules/news/sports';
 import { getFNews } from './modules/news/foreign';
 
-=======
-import {getSNews} from './modules/news/sports';
-import {getFNews} from './modules/news/foreign';
+
 import { openAdminPanel } from './modules/admin/admin';
->>>>>>> 1c7b7d6125007d06171030a7939cb5e484699c80
 
 const body = document.querySelector('body');
 
@@ -22,8 +18,8 @@ document.querySelector('#m-logo').addEventListener('click', () => openAdminPanel
 
 let counter = 0;
 let timer = setInterval(() => {
-        getHsl();
-  }, 60000);
+    getHsl();
+}, 60000);
 
 let newsCounter = 0;
 let timeNews = setInterval(() => {
@@ -54,8 +50,5 @@ getWeather();
 // getNews();
 getLunchMenu();
 
-<<<<<<< HEAD
 export { body };
-=======
-export { body, getLunchMenu, getHsl };
->>>>>>> 1c7b7d6125007d06171030a7939cb5e484699c80
+export { getLunchMenu, getHsl };
