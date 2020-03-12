62656e4c8e78a980450e43320168ac55973606b2
fannyj0 <49899334+fannyj0@users.noreply.github.com>
2020-03-12 18:54:22 +0200

    Update news.js

--- src/modules/news/news.js
+++ src/modules/news/news.js
@@ -2,7 +2,7 @@ import { getJsonData } from '../fetch-module';
 import {showNews} from './news-show1';
 
 const getAltNews = async() => {
-    const url = 'http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=818b12af0ccc48dbb9176013a960243b';
+    const url = 'http://newsapi.org/v2/everything?domains=wsj.com&apiKey=818b12af0ccc48dbb9176013a960243b';
     const response = await getJsonData(url, true);
 
     const news = await response;
